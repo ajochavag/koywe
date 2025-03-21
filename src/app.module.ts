429f1d94a73d22fad8baa3255bef15343ef56f71
Juansebayala <juansebastianayala20@gmail.com>
2025-03-21 11:17:47 -0300

    Create Quote module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { QuoteModule } from './modules/quote/quote.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot(), QuoteModule],
   controllers: [AppController],
   providers: [AppService],
 })
