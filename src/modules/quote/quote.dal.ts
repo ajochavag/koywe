be7ae4f9bec60e4c7bd02283369f92083fc35d1b
Agus-Albarracin <abusscin_@hotmail.com>
2025-04-09 00:21:12 -0300

    refactor(quote): Se integro la URL desde las variables de entorno

--- src/modules/quote/quote.dal.ts
+++ src/modules/quote/quote.dal.ts
@@ -4,12 +4,16 @@
  * **/
 import { Injectable } from '@nestjs/common';
 import axios from 'axios';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class QuoteDAL {
+  constructor(private configService: ConfigService) {}
+
   async getExchangeRate(from: string, to: string): Promise<number> {
     try {
-      const response = await axios.get(`https://api.exchange.cryptomkt.com/api/3/public/price/rate`, {
+      const baseUrl = this.configService.get<string>('CRYPTOMKT_API_URL');
+      const response = await axios.get(`${baseUrl}/public/price/rate`, {
         params: { from: to, to: from } 
       });
 
