c3aab37bbc8465b823a867987dbaa59e1a3d3ac3
Agus-Albarracin <abusscin_@hotmail.com>
2025-04-09 00:09:06 -0300

    fix(quote): Se elimino el controller y service de app

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { QuoteModule } from './modules/quote/quote.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [QuoteModule],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
