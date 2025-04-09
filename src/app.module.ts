c86f368d7a63f94965923399cba21d14486d76fa
Agus-Albarracin <abusscin_@hotmail.com>
2025-04-09 05:30:57 -0300

    feat: Se agrego el modulo de prisma

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
 import { QuoteModule } from './modules/quote/quote.module';
-import { PrismaModule } from './prisma/prisma.module'
+import { PrismaModule } from './modules/prisma/prisma.module'
 
 @Module({
   imports: [QuoteModule, PrismaModule],
