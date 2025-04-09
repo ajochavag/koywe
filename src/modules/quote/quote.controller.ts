1dbffdff033fd8d8b3bbaecf3d6b69e4cf5b7325
Agus-Albarracin <abusscin_@hotmail.com>
2025-04-09 05:52:50 -0300

    feat(quote.controller): Se agrego el endpoint  GET/:id

--- src/modules/quote/quote.controller.ts
+++ src/modules/quote/quote.controller.ts
@@ -1,14 +1,23 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import { Controller, Post, Get, Body, Param } from '@nestjs/common';
 import { QuoteDto } from './dto/quote.dto';
 import { QuoteService } from './quote.service';
+import { QuoteRepository } from './quote.repository';
 
 @Controller('quote')
 export class QuoteController {
-  constructor(private readonly quoteFacade: QuoteService) {}
+  constructor(
+    private readonly quoteFacade: QuoteService,
+    private readonly quoteRepo: QuoteRepository
+  ) {}
 
   @Post()
   async createQuote(@Body() dto: QuoteDto) {
     return this.quoteFacade.createQuote(dto);
+  }
+
+  @Get(':id')
+  async getQuote(@Param('id') id: string) {
+    return this.quoteRepo.getQuoteById(id);
   }
 }
 
