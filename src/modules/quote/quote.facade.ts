fcf98906a2c4d2b2fe64934132b7a8183e993aa0
Agus-Albarracin <abusscin_@hotmail.com>
2025-04-09 03:55:40 -0300

    refactor: renombramos quote.facade a quote.service por su funcion de intermediario entre el controlador y las distintas capa

--- src/modules/quote/quote.facade.ts
+++ src/modules/quote/quote.facade.ts
@@ -1,17 +1,22 @@
 import { Injectable } from '@nestjs/common';
 import { QuoteDto } from './dto/quote.dto';
 import { QuoteBLL } from './quote.bll';
-import { QuoteDAL } from './quote.dal';
+import { QuoteRepository } from './quote.repository';
+import { QuoteProvider } from './quote.provider';
 
 @Injectable()
 export class QuoteFacade {
   constructor(
     private readonly bll: QuoteBLL,
-    private readonly dal: QuoteDAL,
+    private readonly dal: QuoteRepository,
+    private readonly provider: QuoteProvider,
+    
   ) {}
 
   async createQuote(dto: QuoteDto) {
-    const rate = await this.dal.getExchangeRate(dto.from, dto.to);
-    return this.bll.calculateQuote(dto, rate);
+    const rate = await this.provider.getExchangeRate(dto.from, dto.to);
+    const quote = this.bll.calculateQuote(dto, rate);
+    await this.dal.createQuote(quote);
+    return quote;
   }
 }
