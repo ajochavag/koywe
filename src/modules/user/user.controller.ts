d6a8210a7c0f2545a06a5a3d8a5444790fa18003
Agus-Albarracin <abusscin_@hotmail.com>
2025-04-10 04:59:41 -0300

    feat(User): Se crea el modulo User

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -1,9 +1,7 @@
 import { Controller, Get, Req, UseGuards } from '@nestjs/common';
-import { JwtAuthGuard } from '../../authentication/guard/jwt-auth.guard';
 
 @Controller('user')
 export class UserController {
-  @UseGuards(JwtAuthGuard)
   @Get('me')
   async getProfile(@Req() req) {
     return req.user;
