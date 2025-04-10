21423dc36528304b7967c2c3dc725f62125c7c99
Agus-Albarracin <abusscin_@hotmail.com>
2025-04-10 05:21:02 -0300

    feat: Se agregaron los modulos de autenticación y usuario

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { QuoteModule } from './modules/quote/quote.module';
 import { PrismaModule } from './modules/prisma/prisma.module'
+import { AuthModule } from './authentication/auth.module';
+import { UserModule } from './modules/user/user.module';
 
 @Module({
-  imports: [QuoteModule, PrismaModule],
+  imports: [QuoteModule, PrismaModule, AuthModule, UserModule],
   controllers: [],
   providers: [],
 })
