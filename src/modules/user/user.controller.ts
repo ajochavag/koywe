b592801d52bd1a65e49d2ccef5c90fc2d9d80315
Agus-Albarracin <abusscin_@hotmail.com>
2025-04-10 05:15:00 -0300

    feat: Guard añadido

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -1,7 +1,9 @@
 import { Controller, Get, Req, UseGuards } from '@nestjs/common';
+import { JwtAuthGuard } from '../../authentication/guard/jwt-auth.guard';
 
 @Controller('user')
 export class UserController {
+  @UseGuards(JwtAuthGuard)
   @Get('me')
   async getProfile(@Req() req) {
     return req.user;
