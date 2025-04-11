432ad41cd5612b06a32766c5687ed4842f2309bb
Agus-Albarracin <abusscin_@hotmail.com>
2025-04-11 05:14:46 -0300

    refactor: Se quitó User de la interface

--- client/src/models/auth/AuthResponse.tsx
+++ client/src/models/auth/AuthResponse.tsx
@@ -1,11 +1,7 @@
-import User from '../user/user';
-
 export interface SignupResponse {
   message: string;
-  user: User;
 }
 
 export interface SigninResponse {
   access_token: string;
-  user: User;
 }
