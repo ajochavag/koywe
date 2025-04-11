89146402518bb397ea1110eb3185ec6d1c7ddad1
Agus-Albarracin <abusscin_@hotmail.com>
2025-04-11 10:07:39 -0300

    feat(strategy): Se agrego validaciones en caso de token incompleto

--- server/src/authentication/strategies/jwt-token.strategy.ts
+++ server/src/authentication/strategies/jwt-token.strategy.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
+import { UnauthorizedException } from '@nestjs/common';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
@@ -12,6 +13,9 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
   }
 
   async validate(payload: { sub: string; email: string }) {
+    if (!payload || !payload.sub || !payload.email) {
+      throw new UnauthorizedException('Token inválido o incompleto');
+    }
     return payload;
   }
 }
