4f67a1b3456656a611d4483cfae00fd9fadcfacd
Agus-Albarracin <abusscin_@hotmail.com>
2025-04-11 05:22:06 -0300

    feat(quote): Se agregaron las interfaces de Request y Response

---
+++ client/src/models/quote/quote.ts
@@ -0,0 +1,17 @@
+export interface QuoteRequest {
+    amount: number;
+    from: string;
+    to: string;
+  }
+  
+  export interface QuoteResponse {
+    id: string;
+    from: string;
+    to: string;
+    amount: number;
+    rate: number;
+    convertedAmount: number;
+    timestamp: string;
+    expiresAt: string;
+  }
+  
