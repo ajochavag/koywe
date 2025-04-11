0b91bb17544ffef1de255cbf62c2f39d2d9fbba9
Agus-Albarracin <abusscin_@hotmail.com>
2025-04-11 03:07:09 -0300

    feat(Home): Se crea Home, vacio actualmente

---
+++ client/src/pages/Home.tsx
@@ -0,0 +1,13 @@
+'use client';
+import { useState } from 'react';
+import { useForm } from 'react-hook-form';
+import { useNavigate} from 'react-router-dom';
+
+
+export default function Home() {
+
+  return (
+    <>
+    </>
+  );
+}
