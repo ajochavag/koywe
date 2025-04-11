9f4c59fd93e53825b3fb5fa92d0f58d83aef1714
Agus-Albarracin <abusscin_@hotmail.com>
2025-04-11 05:54:43 -0300

    refactor: Se agrego la URL desde el archivo .env.local

--- client/src/services/AuthService.ts
+++ client/src/services/AuthService.ts
@@ -1,16 +1,16 @@
 import axios from 'axios';
 import { SignupResponse, SigninResponse } from '@/models/auth/AuthResponse';
 
-const API_URL = 'http://localhost:8000';
+const BASE_URL = process.env.NEXT_PUBLIC_API_URL!;
 
 export const authService = {
   async login(username: string, password: string): Promise<SigninResponse> {
-    const response = await axios.post(`${API_URL}/auth/signin`, { username, password });
+    const response = await axios.post(`${BASE_URL}/auth/signin`, { username, password });
     return response.data;
   },
 
   async register(email: string, password: string, username: string): Promise<SignupResponse> {
-    const response = await axios.post(`${API_URL}/auth/signup`, {
+    const response = await axios.post(`${BASE_URL}/auth/signup`, {
       email,
       password,
       username,
--- client/src/services/QuoteService.ts
+++ client/src/services/QuoteService.ts
@@ -2,10 +2,10 @@
 import axios from 'axios';
 import { QuoteRequest, QuoteResponse } from '@/models/quote/quote';
 
-const BASE_URL = 'http://localhost:8000/quote';
+const BASE_URL = process.env.NEXT_PUBLIC_API_URL!;
 
 export const createQuote = async (data: QuoteRequest): Promise<QuoteResponse> => {
-  const response = await axios.post(BASE_URL, data, {
+  const response = await axios.post(`${BASE_URL}/quote`, data, {
     headers: {
       Authorization: `Bearer ${localStorage.getItem('KOWEY-JHjskEJEk3ksojñ-SWAP')}`,
     },
@@ -14,7 +14,7 @@ export const createQuote = async (data: QuoteRequest): Promise<QuoteResponse> =>
 };
 
 export const getQuoteById = async (id: string): Promise<QuoteResponse> => {
-  const response = await axios.get(`${BASE_URL}/${id}`, {
+  const response = await axios.get(`${BASE_URL}/quote/${id}`, {
     headers: {
       Authorization: `Bearer ${localStorage.getItem('KOWEY-JHjskEJEk3ksojñ-SWAP')}`,
     },
