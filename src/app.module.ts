d1637cbe1d8effbad0f60d02c6867ffd95b3966e
Agus-Albarracin <abusscin_@hotmail.com>
2025-04-10 23:23:46 -0300

    feat(Thorttle): Se agregaron limites de seguridad para controlar las solicitudes y tiempo de ejecución de las mismas

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,26 @@
+/**
+ *  AppModuel.
+ * 
+ * Thorttle es configurado de manera global para todos los endpoints, para limitar tiempo y solicitudes.
+ * 
+ * Si se desea usar el guard en un controlador para sobrescribir ttl y limit se debería
+ * import { Throttle } from '@nestjs/throttler'; y luego en el endpoint utilizar:
+ * `@Throttle(5, 10)` // Limita 5 peticiones cada 10 segundos, por ejemplo.
+*/
 import { Module } from '@nestjs/common';
+import { ThrottlerModule } from '@nestjs/throttler';
 import { QuoteModule } from './modules/quote/quote.module';
 import { PrismaModule } from './modules/prisma/prisma.module'
 import { AuthModule } from './authentication/auth.module';
 import { UserModule } from './modules/user/user.module';
 
 @Module({
-  imports: [QuoteModule, PrismaModule, AuthModule, UserModule],
+  imports: [QuoteModule, PrismaModule, AuthModule, UserModule,
+    ThrottlerModule.forRoot([{
+      ttl: 60000, //Limita el tiempo en el que se ejecutan las solicitudes.
+      limit: 10, // Limita la cantidad de solicitudes.
+    }]),
+  ],
   controllers: [],
   providers: [],
 })
