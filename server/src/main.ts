52599ff1e669b84d5a0998d2bb00b9747d9ddbf3
Agus-Albarracin <abusscin_@hotmail.com>
2025-04-11 08:29:09 -0300

    refactor: Se agrego un mensaje de incio del servidor

--- server/src/main.ts
+++ server/src/main.ts
@@ -42,7 +42,18 @@ async function bootstrap() {
 
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
-  await app.listen(process.env.PORT_SERVER ?? 8000);
+  await app.listen(process.env.PORT_SERVER ?? 8000,
+    () => { console.log (`
+╔═════════════════════════════════════════════════╗
+
+       🟢 SERVIDOR ONLINE EN EL PUERTO: ${process.env.PORT_SERVER}
+       📅 Fecha: ${new Date().toLocaleString()}
+       
+       ☕ Que tengas un lindo día...
+
+╚═════════════════════════════════════════════════╝
+      `)}
+  );
 }
 
 bootstrap();
