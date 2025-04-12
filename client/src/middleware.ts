f4d98b57fe66129796a25a55df8c4d976e6756f0
Agus-Albarracin <abusscin_@hotmail.com>
2025-04-12 05:36:15 -0300

    feat(middleware): Se agrego middleware para validación de JWT y manejar la privacidad de las rutas

---
+++ client/src/middleware.ts
@@ -0,0 +1,37 @@
+import { NextResponse } from 'next/server'
+import { jwtVerify } from 'jose'
+import type { NextRequest } from 'next/server'
+
+const SECRET_KEY = process.env.JWT_SECRET;
+
+export async function middleware(request: NextRequest) {
+  const token = request.cookies.get('KOWEY-TOKEN-SWAP')?.value;
+  const pathname = request.nextUrl.pathname;
+
+  // Si no hay token y se quiere acceder a /home, redirige
+  if (!token && pathname.startsWith('/home')) {
+    return NextResponse.redirect(new URL('/', request.url));
+  }
+
+  // Si hay token, lo verificamos
+  if (token) {
+    try {
+      // Verificar el token usando `jose`
+      await jwtVerify(token, new TextEncoder().encode(SECRET_KEY));
+
+      // Si el token es válido, continúa
+      return NextResponse.next();
+    } catch (err) {
+      // Si el token no es válido, redirige al login
+      console.error('Token inválido', err);
+      return NextResponse.redirect(new URL('/', request.url));
+    }
+  }
+
+  // Si el token es válido o no es necesario, deja pasar la petición
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: ['/((?!api|_next|.*\\..*).*)'],
+}
