22942425c365029a773395afdbd549873cf132f9
ajochavag <alejandro.jochava@gmail.com>
2025-06-01 21:45:35 -0400

    feat: main and app module

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,25 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { AppModule } from './app/server/app.module';
+
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ExceptionsFilter } from './app/server/exceptions.filter';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalFilters(new ExceptionsFilter());
+
+  const config = new DocumentBuilder()
+    .setTitle('Quote Api')
+    .setDescription('Api to create and get quotes')
+    .addBearerAuth()
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
